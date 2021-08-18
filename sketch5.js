ab2b81c6ce550c32f470e4b4ed5aa30fb84d7d77
Parthseh123 <86557891+Parthseh123@users.noreply.github.com>
2021-08-18 21:57:12 +0530

    Update sketch5.js

--- sketch5.js
+++ sketch5.js
@@ -516,7 +516,7 @@ function draw() {
             player.x=120;
              player.y=520;
 
-             window.open("http://127.0.0.1:5505/home6.html","_self");
+             window.open("https://parthseh123.github.io/game3/home6.html","_self");
              winsound.play();
 
                 
